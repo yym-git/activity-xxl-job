1074a808aa1ad36e62e8092bf06a385a7503c14b
yym-git <1062063192@qq.com>
2022-07-23 17:35:17 +0800

    新增文件TestDev

---
+++ src/main/java/com/bc/test/job/activityxxljob/test/TestDev.java
@@ -0,0 +1,12 @@
+package com.bc.test.job.activityxxljob.test;
+
+/**
+ * @author ym.y
+ * @description
+ * @date 17:34 2022/7/23
+ */
+public class TestDev {
+    public static void main(String[] args) {
+        System.out.println("这是dev分支的代码");
+    }
+}
