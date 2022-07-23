c678738f46b7af0285340c0520a4829704fa8907
yym-git <1062063192@qq.com>
2022-07-23 17:28:53 +0800

    新增文件Test2

---
+++ src/main/java/com/bc/test/job/activityxxljob/test/Test2.java
@@ -0,0 +1,12 @@
+package com.bc.test.job.activityxxljob.test;
+
+/**
+ * @author ym.y
+ * @description
+ * @date 17:28 2022/7/23
+ */
+public class Test2 {
+    public static void main(String[] args) {
+        System.out.println("这是uat分支的代码");
+    }
+}
